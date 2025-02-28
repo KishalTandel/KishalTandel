2b4b2e2fa2d4bc32e18419fa617839c75516bae7
KishalTandel <kishaltandel01@gmail.com>
2025-03-01 04:40:43 +0530

    Added a nicely framed array of quotes and authors

--- quotes.js
+++ quotes.js
@@ -1,6 +1,122 @@
-let quotes = {
-    'J. Robert Oppenheimer': 'I am become death, the destroyer of worlds.',
-    'Martin F. Fischer': "We humans are the greatest of earth's parasites.",
-}
+const quotes = [
+    {quote:'" \'Obvious\' is the most dangerous word in mathematics."',
+     author:"Eric Temple Bell"
+    },
 
-let authors =['J. Robert Oppenheimer','Martin F. Fischer']
+    {quote:'"In mathematics the art of proposing a question must be held of higher value than solving it."',
+     author:"Georg Cantor"
+    },
+
+    {quote:'"Mathematics is a language."',
+     author:"Josiah Willard Gibbs"
+    },
+
+    {quote:'"One of the endlessly alluring aspects of mathematics is that its thorniest paradoxes have a way of blooming into beautiful theories."',
+     author:"Philip J. Davis"
+    },
+    
+     
+    {quote:'"The pure mathematician, like the musician, is a free creator of his world of ordered beauty."',
+     author:"Bertrand Russell"
+    },
+
+    {quote:'"Mathematics is a game played according to certain simple rules with meaningless marks on paper."',
+     author:"David Hilbert"
+    },
+
+    {quote:'"Anyone who attempts to generate random numbers by deterministic means is, of course, living in a state of sin."',
+     author:"John von Neumann"
+    },
+
+    {quote:'"Prediction is very difficult, especially if it\'s about the future."',
+     author:"Niels Bohr"
+    },
+
+    {quote:'"There is a certain sense in which I would say the universe has a purpose. It\'s not there by chance."',
+     author:"Roger Penrose"
+    },
+
+    {quote:'"I think telepathy exists, and I think quantum physics will help us understand its basic properties."',
+     author:"Brian Josephson"
+    },
+
+    {quote:'"Genius is patience."',
+     author:"Isaac Newton"
+    },
+
+    {quote:'"The important thing is not to stop questioning. Curiosity has its own reason for existence."',
+     author:"Albert Einstein"
+    },
+
+    {quote:'"There are children playing in the streets who could solve some of my top problems in physics, because they have modes of sensory perception that I lost long ago."',
+     author:"J. Robert Oppenheimer"
+    },
+
+    {quote:'"It doesn\'t matter how beautiful your theory is, it doesn\'t matter how smart you are. If it doesn\'t agree with experiment, it\'s wrong."',
+     author:"Richard P. Feynman"
+    },
+
+    {quote:'"Intelligence is the ability to adapt to change."',
+     author:"Stephen Hawking"
+    },
+
+    {quote:'"A good programmer is someone who always looks both ways before crossing a one-way street."',
+     author:"Doug Linder"
+    },
+
+    {quote:'"Everybody should learn to program a computer, because it teaches you how to think."',
+     author:"Steve Jobs"
+    },
+
+    {quote:'"Any fool can write code that a computer can understand. Good programmers write code that humans can understand."',
+     author:"Martin Fowler"
+    },
+
+    {quote:'"A computer is like a violin. You can imagine it making beautiful music, but you have to learn how to play it."',
+     author:"Bill Gates"
+    },
+
+    {quote:'"The people who are crazy enough to think they can change the world are the ones who do."',
+     author:"Steve Jobs"
+    },
+
+    {quote:'"Ideas are cheap. It\'s the execution that matters."',
+     author:"Mark Zuckerberg"
+    },
+
+    {quote:'"A topologist is one who doesn\'t know the difference between a doughnut and a coffee cup."',
+     author:"John Kelley"
+    },
+
+    {quote:'"Mathematical proofs, like diamonds, are hard and clear, and will be touched with nothing but strict reasoning."',
+     author:"John Locke"
+    },
+
+    {quote:'"Mathematics consists of proving the most obvious thing in the least obvious way."',
+     author:"George Polye"
+    },
+
+    {quote:'"Mathematics is the art of giving the same name to different things."',
+     author:"Jules Henri Poincare"
+    },
+
+    {quote:'"Perfect numbers like perfect men are very rare."',
+     author:"Rene Descartes"
+    },
+
+    {quote:'"The art of doing mathematics consists in finding that special case which contains all the germs of generality."',
+     author:"David Hilbert"
+    },
+
+    {quote:'"It is not worth an intelligent man\'s time to be in the majority. By definition, there are already enough people to do that."',
+     author:"G.H. Hardy"   
+    },
+
+    {quote:'"You miss 100% of the shots you don’t take."',
+     author:"Wayne Gretzky"
+    },
+
+    {quote:"+-/~^&"+'\n'+'"You are not expected to understand this."',
+     author:""
+    }
+] 
